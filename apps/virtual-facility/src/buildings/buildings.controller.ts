0d653e190101e4add6ee5e46ecf51d8c3d0c75f9
jayce.thai <phat.tnh@carehealth.io>
2024-11-06 10:47:59 +0700

    Init workflows service

---
+++ apps/virtual-facility/src/buildings/buildings.controller.ts
@@ -0,0 +1,58 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { BuildingsService } from './buildings.service';
+import { CreateBuildingDto } from './dto/create-building.dto';
+import { UpdateBuildingDto } from './dto/update-building.dto';
+import axios from 'axios';
+
+@Controller('buildings')
+export class BuildingsController {
+  constructor(private readonly buildingsService: BuildingsService) {}
+
+  @Post()
+  async create(@Body() createBuildingDto: CreateBuildingDto) {
+    return await this.createWorkflow(1);
+    return this.buildingsService.create(createBuildingDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.buildingsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.buildingsService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id') id: string,
+    @Body() updateBuildingDto: UpdateBuildingDto,
+  ) {
+    return this.buildingsService.update(+id, updateBuildingDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.buildingsService.remove(+id);
+  }
+
+  async createWorkflow(buildingId: number) {
+    const res = await axios.post('http://localhost:3001/workflows', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify({ name: 'My Workflow', buildingId }),
+    });
+    return res.data;
+  }
+}
